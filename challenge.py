5b62551d8e36af5dc8fcd620c42cdb9c9a240c81
Bl4ckNblue <abelmnzcc@gmail.com>
2023-12-01 19:04:48 -0500

    Agregué challenge.py

---
+++ challenge.py
@@ -0,0 +1,28 @@
+#RETO 0
+"""
+for i in range (1,101):
+    if i%3 == 0 and i%5 == 0:
+        i = "fizzbuzz"
+    elif i%3 == 0:
+        i = "fizz"
+    elif i%5 == 0:
+        i = "buzz"
+    else:
+        i = i
+    print ("\n",i)
+"""
+
+#RETO 1 
+"""
+word1 = input("Ingresa una palabra: ")
+word2 = input("Ingresa una palabra: ")
+
+if word1 == word2:
+    print (False)
+else:
+    print (True)    
+"""
+
+#RETO 2
+
+
