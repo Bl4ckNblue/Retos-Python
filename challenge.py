0e0f131be94b4a3df1124024cb9331f82b052a5a
Bl4ckNblue <abelmnzcc@gmail.com>
2023-12-02 01:25:28 -0500

    Actualizacion reto 6

--- challenge.py
+++ challenge.py
@@ -32,11 +32,8 @@ for i in range (0,51):
     i = var0+var1
     var0 = var1
     var1 = i
-"""
-  
+ 
 #RETO 3
-
-
 
 for var in range (1,101):
     divisores = 0
@@ -45,7 +42,44 @@ for var in range (1,101):
             divisores+=1
     if divisores == 2:
         print (f"{var} es un numero primo")
+
+#RETO 4
+
+poli = input("Ingrese el poligono del cual hallara el area(triangulo,cuadrado,rectangulo): ")
+
+if poli.lower() == "triangulo": 
+    base = int(input("Ingrese la base: "))
+    altura = int(input("Ingrese la altura: "))
     
+    print (f"El area es {(base*altura)/2}")
+       
+elif poli.lower() == "cuadrado":
+    lado = int(input("Ingrese el lado: "))
+    
+    print (f"El area es {lado**2}")
+
+elif poli.lower() == "rectangulo":
+    base = int(input("Ingrese la base: "))
+    altura = int(input("Ingrese la altura: "))
+    
+    print (f"El area es {base*altura}")
+"""
+#RETO 6
+
+inver = input("Ingresa una frase o palabra: ")
+
+print("".join(reversed(inver)))
+
+
+
+
+    
+    
+
+    
+    
+
+       
 
  
     
