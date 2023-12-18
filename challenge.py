eb9d030dad004b84e7a97c0261bf173d3642508f
Bl4ckNblue <abelmnzcc@gmail.com>
2023-12-18 17:07:40 -0500

    Actualizacion

--- challenge.py
+++ challenge.py
@@ -63,18 +63,65 @@ elif poli.lower() == "rectangulo":
     altura = int(input("Ingrese la altura: "))
     
     print (f"El area es {base*altura}")
-"""
+
 #RETO 6
 
 inver = input("Ingresa una frase o palabra: ")
 
 print("".join(reversed(inver)))
 
+#RETO 7
+st = "Hola me llamo hola"
+tex = st.split()
+frecuencia = {}
 
-
-
+for palabra in tex:
+    palabra = palabra.lower()  # Convertir a minúsculas para considerar mayúsculas y minúsculas como iguales
+    if palabra in frecuencia:
+        frecuencia[palabra] += 1
+    else:
+        frecuencia[palabra] =2+
+#RETO 8    
     
-    
+n = 255
+b = []
+while n > 0:
+    residuo = n % 2
+    b.append(str(residuo))
+    n = n // 2
+b.reverse()
+
+st = ' '.join(b)
+print (st)
+
+
+#RETO 9
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+}
+
+tex = input("Ingrese un texto: ")
+morse = " "
+for char in tex.upper():
+    if char == " ":
+        morse += " "
+    elif char in MORSE_CODE_DICT:
+        morse += MORSE_CODE_DICT[char] + " "
+    else:
+        morse += char
+
+print (morse)    
+"""
+
+#RETO 10 
+
+
 
     
     
